f709c3ec6c7b3ba7783e42f75a5ffcaad44eb7fb
Karina Velasquez <kvelasquez@xpanxion.com>
2023-07-11 21:51:21 -0400

    Demo

--- Demo.js
+++ Demo.js
@@ -1,3 +1,15 @@
-let k = "Hey Karina";
+const question1 = "What is your favorite color?";
+const answer1 = "";
+console.log(question1);
+console.log(answer1);
 
-console.log(k)
+const question2 = "What is your favorite hobby?";
+const answer2 = "";
+console.log(question2);
+console.log(answer2);
+
+
+const question3 = "What is your favorite song?"
+const answer3 = ""
+console.log(question3)
+console.log(answer3)
